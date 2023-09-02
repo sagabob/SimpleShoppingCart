cefba12f06f18a05b7ed46683442ddbc0d30d63a
DESKTOP-H29UOQ1\bobpham <bobpham.tdp@gmail.com>
2023-09-03 02:29:21 +1000

    work in progress

---
+++ TdpShop/src/TdpShop.Web/Controllers/CouponController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TdpShop.Web.Services;
+
+namespace TdpShop.Web.Controllers;
+
+public class CouponController : Controller
+{
+    private readonly ICouponServices _couponServices;
+
+    public CouponController(ICouponServices couponServices)
+    {
+        _couponServices = couponServices;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var listOfCoupons = await _couponServices.GetAllCouponsAsync();
+
+
+        return View(listOfCoupons);
+    }
+}
